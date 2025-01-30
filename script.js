c7730b23716aee497429e2f2f85ebfcc18177b2e
kartik120-pk <118261121+kartik120-pk@users.noreply.github.com>
2025-01-30 14:56:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function capitalName(){
+	var inputfield = document.getElementById("fname");
+	inputfield.value = inputfield.value.toUpperCase();
+}
+document.getElementById("fname").addEventListener("input", capitalName);
